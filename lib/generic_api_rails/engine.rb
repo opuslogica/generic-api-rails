b1e9a8d058bb61e5fc8924c51ace1a0038d19964
Daniel Staudigel <dstaudigel@gmail.com>
2014-04-18 16:57:07 -0700

    Add the engine references so that the gem properly loads migrations.

--- lib/generic_api_rails/engine.rb
+++ lib/generic_api_rails/engine.rb
@@ -1,5 +1,15 @@
 module GenericApiRails
   class Engine < Rails::Engine
     engine_name 'generic_api_rails'
+
+    initializer :append_migrations do |app|
+
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+
+      end
+    end
   end
 end
