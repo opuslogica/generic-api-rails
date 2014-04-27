a74c44ed249b3877dd7fb241fcbef66794f3f99a
Brian J. Fox <bfox@OpusLogica.com>
2014-04-26 18:57:36 -0700

    This isn't BZU code.

--- app/controllers/generic_api_rails/base_controller.rb
+++ app/controllers/generic_api_rails/base_controller.rb
@@ -15,7 +15,7 @@ module GenericApiRails
       response.headers['Access-Control-Allow-Credentials'] = 'true'
       response.headers['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE, OPTIONS, PATCH'
       response.headers['Access-Control-Max-Age'] = "1728000"
-      response.headers['Access-Control-Allow-Headers'] = 'X-CSRF-Token, X-Requested-With, X-Prototype-Version, content-type, api-token, bzu-client-id'
+      response.headers['Access-Control-Allow-Headers'] = 'X-CSRF-Token, X-Requested-With, X-Prototype-Version, content-type, api-token'
     end
 
     # If this is a preflight OPTIONS request, then short-circuit the
@@ -25,7 +25,7 @@ module GenericApiRails
       if request.method == "OPTIONS"
         headers['Access-Control-Allow-Origin'] = '*'
         headers['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE, OPTIONS, PATCH'
-        headers['Access-Control-Allow-Headers'] = 'X-CSRF-Token, X-Requested-With, X-Prototype-Version, content-type, api-token, bzu-client-id'
+        headers['Access-Control-Allow-Headers'] = 'X-CSRF-Token, X-Requested-With, X-Prototype-Version, content-type, api-token'
         headers['Access-Control-Max-Age'] = '1728000'
         render :text => '', :content_type => 'text/plain'
       end
