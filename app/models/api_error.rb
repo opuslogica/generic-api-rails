c029a31f16a17ca4c8af8cf1f7ee6ca373d9b6b5
Brian J. Fox <bfox@OpusLogica.com>
2014-04-26 18:58:37 -0700

    Use 401 for bad authentication, use 403 for lack of permission

--- app/models/api_error.rb
+++ app/models/api_error.rb
@@ -10,9 +10,9 @@ class ApiError < ActiveRecord::Base
     [
      { :code => INVALID_ARGUMENT, :description => "Invalid argument", :status_code => 403 },
      { :code => INVALID_RANGE, :description => "Invalid range argument - missing beg or end", :status_code => 403 },
-     { :code => INVALID_API_TOKEN, :description => "Invalid API token", :status_code => 403 },
-     { :code => INVALID_USERNAME_OR_PASSWORD, :description => "Invalid username or password", :status_code => 403 },
-     { :code => INVALID_PERSON_ID, :description => "Invalid person id", :status_code => 403 },
+     { :code => INVALID_API_TOKEN, :description => "Invalid API token", :status_code => 401 },
+     { :code => INVALID_USERNAME_OR_PASSWORD, :description => "Invalid username or password", :status_code => 401 },
+     { :code => INVALID_PERSON_ID, :description => "Invalid person id", :status_code => 401 },
      { :code => UNAUTHORIZED, :description => "Unauthorized for this action", :status_code => 403 },
     ]
 
