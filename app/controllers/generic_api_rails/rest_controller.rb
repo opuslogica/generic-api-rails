0b4feb5ee99f8cf084eb07b8e4f285486426427e
Daniel Staudigel <dstaudigel@gmail.com>
2014-05-11 10:20:20 -0700

    use the to-hash trick to circumvent the mass-assignment prevention

--- app/controllers/generic_api_rails/rest_controller.rb
+++ app/controllers/generic_api_rails/rest_controller.rb
@@ -98,7 +98,7 @@ module GenericApiRails
       hash = params['rest']
 
       r = @model.find(params[:id])
-      r.update_attributes(hash)
+      r.update_attributes(hash.to_hash)
 
       render_error(ApiError::UNAUTHORIZED) and return false unless authorized?(:update, r)
 
