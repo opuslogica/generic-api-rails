25d5ade6b22b4ed37c5c8401ac9d523891ff9013
Daniel Staudigel <dstaudigel@gmail.com>
2014-06-10 21:10:20 -0700

    Make username not required.

--- app/controllers/generic_api_rails/authentication_controller.rb
+++ app/controllers/generic_api_rails/authentication_controller.rb
@@ -91,7 +91,7 @@ class GenericApiRails::AuthenticationController < GenericApiRails::BaseControlle
     if incoming_api_token.present? and not password
       api_token = ApiToken.find_by(token: incoming_api_token) rescue nil
       @credential = api_token.credential if api_token
-      (api_token.destroy and api_token = nil) if (not @credential) or (@credential.email.address != username)
+      (api_token.destroy and api_token = nil) if (not @credential) or (username && @credential.email.address != username)
     end
 
     if not api_token
