07745b28b071122930c17e17c2cb665207a3efbd
Brian J. Fox <bfox@OpusLogica.com>
2014-06-17 08:18:02 -0700

    Readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 GenericApiRails::Engine.routes.draw do
-  namespace :generic_api_rails , :path => "/" do
+  namespace :generic_api_rails, :path => "/" do
     match '*path' => "base#options", :via => [:options]
   
     namespace "authentication" do 
@@ -11,13 +11,13 @@ GenericApiRails::Engine.routes.draw do
       post 'signup'
     end
 
-    get 'version' => 'misc#version'
-    get 'whoami' => 'misc#whoami'
+    get    'version'                    => 'misc#version'
+    get    'whoami'                     => 'misc#whoami'
 
-    get '(:namespace/):model/:id' => 'rest#read',:constraints => { :id => /\d+/  , :model => /\D+/ }
-    match '(:namespace/):model/:id' => 'rest#update' , :via => [:post,:put,:patch],:constraints => { :id => /\d+/ , :model => /\D+/ }
-    delete '(:namespace/):model/:id' => 'rest#destroy',:constraints => { :id => /\d+/  , :model => /\D+/}
-    get '(:namespace/):model' => 'rest#index', :constraints => { :id => /\d+/ , :model => /\D+/} 
-    post '(:namespace/):model' => 'rest#create',:constraints => { :id => /\d+/ , :model => /\D+/}
+    get    '(:namespace/):model/:id'    => 'rest#read',    :constraints: { :id => /\d+/, :model => /\D+/ }
+    match  '(:namespace/):model/:id'    => 'rest#update',  :constraints: { :id => /\d+/, :model => /\D+/ }, via: [:post, :put, :patch]
+    delete '(:namespace/):model/:id'    => 'rest#destroy', :constraints: { :id => /\d+/, :model => /\D+/ }
+    get    '(:namespace/):model'        => 'rest#index',   :constraints: { :id => /\d+/, :model => /\D+/ }
+    post   '(:namespace/):model'        => 'rest#create',  :constraints: { :id => /\d+/, :model => /\D+/ }
   end
 end
