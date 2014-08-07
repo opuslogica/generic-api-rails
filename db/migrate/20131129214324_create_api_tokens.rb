03e3da0dab6d7dcd015c617709aaee960ecc4a7a
Brian J. Fox <bfox@OpusLogica.com>
2014-08-07 10:27:54 -0700

    Be nicer to 4 byte databases with indices limited to 767 characters

--- db/migrate/20131129214324_create_api_tokens.rb
+++ db/migrate/20131129214324_create_api_tokens.rb
@@ -2,11 +2,11 @@ class CreateApiTokens < ActiveRecord::Migration
   def change
     create_table :api_tokens do |t|
       t.references :person, index: true
-      t.references :credential, index: true, polymorphic: true
-      
-      t.string :token
-
+      t.string     :credential_type, limit: 64
+      t.integer    :credential_id
+      t.string     :token, limit: 64
       t.timestamps
     end
+    add_index :api_tokens, [:credential_id, :credential_type]
   end
 end
