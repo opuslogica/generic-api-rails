b234723e8c77b8c97ff793361764e75472bdb5c8
Brian J. Fox <bfox@opuslogica.com>
2017-02-10 09:51:40 -0800

    Get rid of deprecated usage of render(text: 'foo')

--- app/controllers/generic_api_rails/base_controller.rb
+++ app/controllers/generic_api_rails/base_controller.rb
@@ -27,7 +27,7 @@ module GenericApiRails
         headers['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE, OPTIONS, PATCH'
         headers['Access-Control-Allow-Headers'] = 'X-CSRF-Token, X-Requested-With, X-Prototype-Version, content-type, api-token, OLI-Device-ID, OLI-Device-Identifier'
         headers['Access-Control-Max-Age'] = '1728000'
-        render(text: '', content_type: 'text/plain')
+        render(plain: '', content_type: 'text/plain')
         false
       end
       true
